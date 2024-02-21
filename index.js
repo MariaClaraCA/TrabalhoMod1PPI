02dde130f73974ef5fd8208e2cfa1640f96e32f7
Maria Clara <mariaclarag.barbosa124@gmail.com>
2024-02-21 18:03:05 -0300

    concluindo

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 import express from 'express';
 import process from 'process';
 import path from 'path';
+import session from 'express-session';
+import autenticar from './seguranca/autenticar.js';
 
 const host = 'localhost';
-const porta = 3003;
+const porta = 3000;
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'M1nH4Ch4v3S3cr3t4',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {  
+        maxAge: 60 * 1000 * 15
+    }
+}))
+
+
+app.post('/login', (requisicao, resposta)=>{
+    const usuario = requisicao.body.usuario;
+    const senha   = requisicao.body.senha;
+    if (usuario && senha && usuario === 'mariaclara@gmail.com' && senha === '123'){
+        requisicao.session.usuarioLogado = true;
+        resposta.redirect('/conteudo.html');
+    }
+    else{
+        resposta.redirect('/telalogin.html');
+    }
+})
 
 app.use(express.static(path.join(process.cwd(), 'publico')));
+app.use(autenticar, express.static(path.join(process.cwd(), 'privado')));
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'publico', 'conteudo.html'));
-});
 
 
 app.listen(porta, host, () => {
