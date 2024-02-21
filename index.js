793689d0f10c1d7ba607f8ca00a9e88c62983249
Maria Clara <mariaclarag.barbosa124@gmail.com>
2024-02-20 23:35:26 -0300

    mudanças

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const porta = 3003;
 
 const app = express();
 
-// Define o middleware para servir conteúdo estático do diretório 'publico'
+
 app.use(express.static(path.join(process.cwd(), 'publico')));
 
-// Rota raiz que serve o arquivo conteudo.html
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'publico', 'conteudo.html'));
 });
 
-// Inicia o servidor
+
 app.listen(porta, host, () => {
     console.log(`Servidor escutando em http://${host}:${porta}`);
 });
---
+++ publico/scripts/script3.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const inputQuantidadeIngressos = document.getElementById('quantidade-ingressos');
+    const totalValue = document.getElementById('total-value');
+    inputQuantidadeIngressos.addEventListener('input', function() {
+        const precoIngresso = 20;
+        const quantidadeIngressos = inputQuantidadeIngressos.value;
+        const total = precoIngresso * quantidadeIngressos;
+        totalValue.textContent = `Total: R$ ${total.toFixed(2)}`;
+    });
+});
